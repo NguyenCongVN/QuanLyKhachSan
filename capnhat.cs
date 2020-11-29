8d489a9863ef361437be974987d747d62f29be59
phamxuantien27 <65357044+phamxuantien27@users.noreply.github.com>
2020-11-29 04:40:39 -0800

    add cap nhat nhe thầy công

---
+++ capnhat.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Qlks
+{
+    public partial class capnhat : Form
+    {
+        public capnhat()
+        {
+            InitializeComponent();
+        }
+
+       
+
+        private void form_load()
+        {
+           
+        }
+
+        private void capnhat_Load(object sender, EventArgs e)
+        {
+            
+        }
+        private void lu_click()
+        {
+           
+        }
+        private void bt_lu_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void bt_huy_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        public void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+           
+        }
+
+        private void bt_qua_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
